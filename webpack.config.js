dca3af5aa09e0ff5d5869f18a53bac4090964f98
Pascal Kleindienst <mail@pascalkleindienst.de>
2016-10-21 09:24:27 +0200

    added webpack+babel

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/saga.js',
+    output: {
+        path: './dist',
+        filename: 'saga-gallery.min.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+            output: {
+                comments: false,
+            },
+        }),
+    ]
+ };
